74d1e5860fbf24032f36d266101e51fa43f7064e
lauren14-08 <laurenkim2008@gmail.com>
2025-03-14 09:19:49 -0400

    Updated API Key

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "4aRfAMguITP6RFb1eEH1PxSyhg9N1qeLdUyJlSx1fUqO5vtZDShee0IIzQPyxrYu	";
+var authKey = "4aRfAMguITP6RFb1eEH1PxSyhg9N1qeLdUyJlSx1fUqO5vtZDShee0IIzQPyxrYu";
 /**
  * Get list of teams in event
  *

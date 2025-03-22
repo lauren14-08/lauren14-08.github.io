fb519b427e32f52c272654ea4c5dcb7275c09186
lauren14-08 <laurenkim2008@gmail.com>
2025-03-22 10:33:34 -0400

    Change authKey

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "4aRfAMguITP6RFb1eEH1PxSyhg9N1qeLdUyJlSx1fUqO5vtZDShee0IIzQPyxrYu";
 /**
  * Get list of teams in event
  *
